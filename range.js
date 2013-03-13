12b7fc8e928ad49f1ca532bcaefcda19313957f8
Arthur Britto <ahbritto@gmail.com>
2013-03-12 22:57:31 -0700

    Remove dead code.

--- range.js
+++ range.js
@@ -55,26 +55,6 @@ Range.segment_tail = function (s) {
   }
 };
 
-//var intersect_raw = function (a, b) {
-//  var result  = new Range;
-//
-//  if (a.length && b.length)
-//  {
-//    var a_head  = Number === typeof a[0] ? a[0] : a[1];
-//    var b_head  = Number === typeof b[0] ? b[0] : b[1];
-//    var a_tail  = Number === typeof a[0] ? a[0] : a[a.length-1];
-//    var b_tail  = Number === typeof b[0] ? b[0] : b[b.length-1];
-//
-//    intersect_bare(a_head, a_tail, b_head, b_tail);
-//
-//    if (a_head < b_head) {
-//      interset_bare(a_
-//    }
-//  }
-//
-//  return result;
-//};
-
 // Return the first value in an array or null.
 Range.prototype.first = function () {
   return this.value.length ? Range.segment_head(this.value[0]) : null ;
